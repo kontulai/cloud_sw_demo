7ff4c7d1cd068f06644fd2a5386e94e36cab1ac0
Jussi Malinen <jussi.malinen@reaktor.fi>
2012-01-23 17:09:02 +0200

    basic server networking

--- server/src/BonkServer.py
+++ server/src/BonkServer.py
@@ -1,2 +1,31 @@
+import threading
+import SocketServer
+
+
 class BonkServer(object):
-    pass
+
+    def __init__(self, ip, port):
+        self._ip = ip
+        self._port = port
+        self._server = None
+
+    def start(self):
+        self._server = BonkServerThreading((self._ip, self._port), BonkRequestHandler)
+        server_thread = threading.Thread(target=self._server.serve_forever)
+        server_thread.daemon = True
+        server_thread.start()
+
+    def stop(self):
+        self._server.shutdown()
+
+
+class BonkRequestHandler(SocketServer.BaseRequestHandler):
+
+    def handle(self):
+        data = self.request.recv(1024)
+        self.request.send('\xff')
+
+
+class BonkServerThreading(SocketServer.ThreadingMixIn, SocketServer.TCPServer):
+
+    allow_reuse_address = True
---
+++ server/utest/test_server_start.py
@@ -0,0 +1,15 @@
+import unittest
+from BonkServer import BonkServer
+from Network import TCPClient
+
+class TestServer(unittest.TestCase):
+
+    def test_server_responds_to_request(self):
+        server = BonkServer('127.0.0.1', 55555)
+        server.start()
+        client = TCPClient(timeout=1)
+        client.connect_to('127.0.0.1', 55555)
+        client.send('\xff\x00')
+        response = client.read()
+        self.assertTrue(response)
+        server.stop()
